e065f826faa6ebd9a9585314ecbdb214e4b18280
Vicente Soriano <vicente.soriano@icloud.com>
2018-09-13 15:05:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
   while i < collection.size
     item = yield collection[i]
     if item == true
-    empty_array << true
+    empty_array << item
     end
     i = i + 1
     binding.pry
