3b9424323ac6b606cacc7e9469abf7029242440e
Vicente Soriano <vicente.soriano@icloud.com>
2018-09-13 15:16:37 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,10 +2,9 @@ def my_select(collection)
   i = 0
   empty_array = []
   while i < collection.size
-    yield collection[i]
-    binding.pry
     if yield collection [i] == true
      empty_array << collection[i]
+    end
     i = i + 1
   end
   empty_array
